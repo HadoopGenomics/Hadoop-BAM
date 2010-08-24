a75d7d00c61ad1fe74b9892fedd5ac91fbaae648
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2010-08-24 15:15:22 +0300

    Use a longer name than _partitioning.
    Necessary for many inputs.

--- Summarize.java
+++ Summarize.java
@@ -172,11 +172,14 @@ public final class Summarize extends Configured implements Tool {
 		Path inputDir = inputFile.getParent();
 		inputDir = inputDir.makeQualified(inputDir.getFileSystem(conf));
 
-		Path partition = new Path(inputDir, "_partitioning");
+		String name = inputFile.getName();
+
+		Path partition = new Path(inputDir, "_partitioning" + name);
 		TotalOrderPartitioner.setPartitionFile(conf, partition);
 
 		try {
-			URI partitionURI = new URI(partition.toString() + "#_partitioning");
+			URI partitionURI = new URI(
+				partition.toString() + "#_partitioning" + name);
 			DistributedCache.addCacheFile(partitionURI, conf);
 			DistributedCache.createSymlink(conf);
 		} catch (URISyntaxException e) { assert false; }
