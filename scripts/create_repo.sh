ddb4652e17a82d8ae8937ce82ccd001c6668cab7
Andre Schumacher <schumach@icsi.berkeley.edu>
2013-10-25 18:30:36 -0700

    added script to package picard, samtools and hadoop-bam as maven repository

---
+++ scripts/create_repo.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Script adapted from
+# https://github.com/massie/adam/blob/master/scripts/create-thirdparty-repo.sh
+
+TMPDIR=/tmp/hadoop_bam_repo
+
+HADOOPBAM_VERSION="6.0"
+PICARD_VERSION="1.93"
+SAMTOOLS_VERSION="$PICARD_VERSION"
+
+rm -rf "$TMPDIR"
+mkdir -p "$TMPDIR"
+$(cd $TMPDIR && wget http://sourceforge.net/projects/hadoop-bam/files/hadoop-bam-${HADOOPBAM_VERSION}.tar.gz)
+$(cd "$TMPDIR" && tar xvpzf hadoop-bam-${HADOOPBAM_VERSION}.tar.gz)
+
+REPO_ID="hadoop-bam-repo"
+OUTPUT_DIR="$TMPDIR/$REPO_ID"
+HADOOP_BAM_PATH="$TMPDIR/hadoop-bam-${HADOOPBAM_VERSION}"
+# Delete any old artifacts
+rm -rf $OUTPUT_DIR
+OUTPUT_URL="file://$OUTPUT_DIR"
+
+mvn deploy:deploy-file -Durl=$OUTPUT_URL -DrepositoryId=$REPO_ID -Dfile=$HADOOP_BAM_PATH/picard-$PICARD_VERSION.jar \
+-DgroupId=picard -DartifactId=picard -Dversion=$PICARD_VERSION -DgeneratePom.description="Picard"
+
+mvn deploy:deploy-file -Durl=$OUTPUT_URL -DrepositoryId=$REPO_ID -Dfile=$HADOOP_BAM_PATH/sam-$SAMTOOLS_VERSION.jar \
+-DgroupId=samtools -DartifactId=samtools -Dversion=$SAMTOOLS_VERSION -DgeneratePom.description="Samtools"
+
+mvn deploy:deploy-file -Durl=$OUTPUT_URL -DrepositoryId=$REPO_ID -Dfile=$HADOOP_BAM_PATH/hadoop-bam-${HADOOPBAM_VERSION}.jar \
+-DgroupId=fi.tkk.ics.hadoop.bam -DartifactId=hadoop-bam -Dversion=$HADOOPBAM_VERSION -DgeneratePom.description="Hadoop-BAM"
+
+for i in $(find $OUTPUT_DIR -type d); do
+    echo "Options +Indexes" > ${i}/.htaccess;
+done
+
+echo "now please push the contents of $OUTPUT_DIR to /home/project-web/hadoop-bam/htdocs/maven on sourceforge"
+
