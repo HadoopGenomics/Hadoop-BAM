f7896531d4167c39392edee32878c0ace64aa6eb
Michael L Heuer <heuermh@acm.org>
2016-05-24 14:01:59 -0500

    Add unit test for VCFHeaderReader, fails on gzipped and BGZF VCF formats

---
+++ src/test/java/org/seqdoop/hadoop_bam/util/TestVCFHeaderReader.java
@@ -0,0 +1,34 @@
+package org.seqdoop.hadoop_bam.util;
+
+import java.io.IOException;
+
+import com.google.common.io.Resources;
+
+import htsjdk.samtools.seekablestream.ByteArraySeekableStream;
+import htsjdk.samtools.seekablestream.SeekableStream;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TestVCFHeaderReader {
+
+  @Test
+  public void testReadHeaderFromVCF() throws IOException {
+    assertNotNull(VCFHeaderReader.readHeaderFrom(seekableStream("test.vcf")));
+  }
+
+  @Test
+  public void testReadHeaderFromGzippedVCF() throws IOException {
+    assertNotNull(VCFHeaderReader.readHeaderFrom(seekableStream("test.vcf.gz")));
+  }
+
+  @Test
+  public void testReadHeaderFromBGZFVCF() throws IOException {
+    assertNotNull(VCFHeaderReader.readHeaderFrom(seekableStream("test.vcf.bgzf.gz")));
+  }
+
+  private SeekableStream seekableStream(final String resource) throws IOException {
+    return new ByteArraySeekableStream(Resources.toByteArray(ClassLoader.getSystemClassLoader().getResource(resource)));
+  }
+}
