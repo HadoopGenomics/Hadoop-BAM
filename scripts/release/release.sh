23437891df86d24a5d5561b291206b81bc7916bb
Frank Austin Nothaft <frank.nothaft@databricks.com>
2018-03-01 09:31:17 -0800

    Add scripts for releasing Hadoop-BAM that allow for releasing off of a branch.

---
+++ scripts/release/release.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# do we have enough arguments?
+if [ $# < 3 ]; then
+    echo "Usage:"
+    echo
+    echo "./release.sh <release version> <development version>"
+    exit 1
+fi
+
+# pick arguments
+release=$1
+devel=$2
+
+# get current branch
+branch=$(git status -bs | awk '{ print $2 }' | awk -F'.' '{ print $1 }' | head -n 1)
+
+commit=$(git log --pretty=format:"%H" | head -n 1)
+echo "releasing from ${commit} on branch ${branch}"
+
+git push origin ${branch}
+
+#do release
+mvn --batch-mode \
+  -Dresume=false \
+  -Dtag=${release} \
+  -DreleaseVersion=${release} \
+  -DdevelopmentVersion=${devel} \
+  -DbranchName=${release} \
+  release:clean \
+  release:prepare \
+  release:perform
+
+if [ $? != 0 ]; then
+  echo "Releasing failed."
+  exit 1
+fi
+
+if [ $branch = "master" ]; then
+  # if original branch was master, update versions on original branch
+  git checkout ${branch}
+  mvn versions:set -DnewVersion=${devel} \
+    -DgenerateBackupPoms=false
+  git commit -a -m "Modifying pom.xml files for new development after ${release} release."
+  git push origin ${branch}
+fi
